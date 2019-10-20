3ef555e5d1228e183805b9a492e3b7d1857adb33
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-21 00:17:36 +0700

    RefactoringForTestability

---
+++ 8_2_RefactoringForTestability/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_2_RefactoringForTestability
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
